9d9302203ed0cb8407a74c0f20bef4b1cd3821ba
MOILLA HEMANTH ESWAR REDDY <86761812+HEMANTHESWARREDDY@users.noreply.github.com>
2024-02-06 22:40:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,109 @@
+// function red(){
+//     document.getElementById("mainHead").style.background="red";
+// }
+
+// function blue(){
+//     document.getElementById("mainHead").style.background="blue";
+// }
+
+// function green(){
+//     document.getElementById("mainHead").style.background="green";
+// }
+
+// function black(){
+//     document.getElementById("mainHead").style.background="black";
+// }
+
+const button1 = document.getElementById('red');
+const body1 = document.getElementById('mainHead');
+
+button1.addEventListener('mouseover', () => {
+    body1.style.backgroundColor = 'red';
+});
+
+button1.addEventListener('mouseout', () => {
+    body1.style.backgroundColor = 'white';
+});
+
+
+const button2 = document.getElementById('blue');
+const body2 = document.getElementById('mainHead');
+
+button2.addEventListener('mouseover', () => {
+    body2.style.backgroundColor = 'blue';
+});
+
+button2.addEventListener('mouseout', () => {
+    body2.style.backgroundColor = 'white';
+});
+
+
+const button3 = document.getElementById('green');
+const body3 = document.getElementById('mainHead');
+
+button3.addEventListener('mouseover', () => {
+    body3.style.backgroundColor = 'green';
+});
+
+button3.addEventListener('mouseout', () => {
+    body3.style.backgroundColor = 'white';
+});
+
+
+const button4 = document.getElementById('black');
+const body4 = document.getElementById('mainHead');
+
+button4.addEventListener('mouseover', () => {
+    body4.style.backgroundColor = 'black';
+});
+
+button4.addEventListener('mouseout', () => {
+    body4.style.backgroundColor = 'white';
+});
+
+
+const button5 = document.getElementById('orange');
+const body5 = document.getElementById('mainHead');
+
+button5.addEventListener('mouseover', () => {
+    body5.style.backgroundColor = 'orange';
+});
+
+button5.addEventListener('mouseout', () => {
+    body5.style.backgroundColor = 'white';
+});
+
+
+const button6 = document.getElementById('yellow');
+const body6 = document.getElementById('mainHead');
+
+button6.addEventListener('mouseover', () => {
+    body6.style.backgroundColor = 'yellow';
+});
+
+button6.addEventListener('mouseout', () => {
+    body6.style.backgroundColor = 'white';
+});
+
+const button7 = document.getElementById('pink');
+const body7 = document.getElementById('mainHead');
+
+button7.addEventListener('mouseover', () => {
+    body7.style.backgroundColor = 'pink';
+});
+
+button7.addEventListener('mouseout', () => {
+    body7.style.backgroundColor = 'white';
+});
+
+
+const button8 = document.getElementById('tomato');
+const body8 = document.getElementById('mainHead');
+
+button8.addEventListener('mouseover', () => {
+    body8.style.backgroundColor = 'tomato';
+});
+
+button8.addEventListener('mouseout', () => {
+    body8.style.backgroundColor = 'white';
+});
